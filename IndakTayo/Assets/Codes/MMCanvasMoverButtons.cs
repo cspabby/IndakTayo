e2a25bc5e61c6e2f605ecb2f7fae199c55d7cb74
MaryDen <marydentoralba100@gmail.com>
2025-04-06 07:10:05 +0800

    index usage
    tracking the visible UI object.

--- IndakTayo/Assets/Codes/MMCanvasMoverButtons.cs
+++ IndakTayo/Assets/Codes/MMCanvasMoverButtons.cs
@@ -9,7 +9,7 @@ public class MMCanvasMoverButtons : MonoBehaviour
 {
 
     public GameObject[] objectsToMove; // Array of UI objects (canvases or panels) to switch between
-    public int currentIndex = 0;
+    public int currentIndex = 0; // Index of the currently active object
     private int previousIndex = 0; // Track the previous index
     public GameObject incrementButton;
     public GameObject decrementButton;
