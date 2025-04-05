c08c11d199c9614be526f1fb9d6ce7ea420f9f60
Abby <cspabby@gmail.com>
2025-04-05 20:44:16 +0800

    AttemptsIncrement

--- IndakTayo/Assets/AttemptsIncrement.cs
+++ IndakTayo/Assets/AttemptsIncrement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AttemptsIncrement : MonoBehaviour
 {
+    // Track the attempts of the player
     public AttemptsValue attempts;
     // Start is called before the first frame update
     void Start()
