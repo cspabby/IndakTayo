76e33a8b354462717aff741a91bd1e31379d9dba
Abby <cspabby@gmail.com>
2025-04-07 22:31:30 +0800

    ShowSlide on HTP

--- IndakTayo/Assets/Codes/ShowSlideHTP.cs
+++ IndakTayo/Assets/Codes/ShowSlideHTP.cs
@@ -11,7 +11,7 @@ public class ShowSlideHTP : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) //Player compare tag
         {
 
             ShowDancePass.ShowDanceSlide(other);
