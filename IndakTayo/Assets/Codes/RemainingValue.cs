af70736c4a8b2f108da95efaf4ee1eab20358456
Abby <cspabby@gmail.com>
2025-04-09 22:14:15 +0800

    added a passing score of 50

--- IndakTayo/Assets/Codes/RemainingValue.cs
+++ IndakTayo/Assets/Codes/RemainingValue.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class RemainingValue : MonoBehaviour
 {
+    public TotalPoints points;
     public TextMeshProUGUI RemainingText; // Assign in Inspector
     public GameObject ButtonAttempts;
     public GameObject NextButton;
@@ -15,7 +16,7 @@ public class RemainingValue : MonoBehaviour
         int attemptsToPass = PlayerPrefs.GetInt("attempts");
         int remaining = 3 - attemptsToPass;
         float percentValue = percent.TaskPercentageValue;
-
+        int score = points.Points;
         if (remaining < 0)
         {
             remaining = 0;
@@ -23,7 +24,7 @@ public class RemainingValue : MonoBehaviour
 
         RemainingText.text = remaining.ToString();
 
-        if (percentValue < 100)
+        if ((percentValue < 100)|| (percentValue <= 100 && score < 50))
         {
             if (remaining == 0)
             {
@@ -34,8 +35,8 @@ public class RemainingValue : MonoBehaviour
             {
                 ButtonAttempts.SetActive(true);
             }
-        }
-        else
+        } 
+        else 
         {
             NextButton.SetActive(true);
         }
